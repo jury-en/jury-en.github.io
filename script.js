4492e9981fc8f2665c1c7c793f8de7d2c21cb776
jurence <jurencenon@gmail.com>
2023-11-14 08:59:08 +0300

    Adding dark mode toggle

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
