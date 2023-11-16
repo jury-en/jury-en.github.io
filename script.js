a62e660ee3b53c1de78e3cb312d9c0fd96dc3772
jurence <jurencenon@gmail.com>
2023-11-16 17:13:44 +0300

    Adding about page

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ function toggleContent(id) {
         subheading.classList.remove('active'); // Remove 'active' class to reset the icon
     }
 }
+
+// <script>
+//     document.addEventListener('DOMContentLoaded', () => {
+//         const navLinks = document.querySelectorAll('.resume-nav a');
+//         navLinks.forEach(link => {
+//             link.addEventListener('click', function(e) {
+//                 e.preventDefault();
+//                 // Remove active class from all links
+//                 navLinks.forEach(link => link.classList.remove('active'));
+//                 // Add active class to clicked link
+//                 this.classList.add('active');
+//                 const targetId = this.getAttribute('href');
+//                 const targetSection = document.querySelector(targetId);
+//                 targetSection.scrollIntoView({
+//                     behavior: 'smooth',
+//                     block: 'start'
+//                 });
+//             });
+//         });
+//     });
+// </script>
+
